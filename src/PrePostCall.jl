4c47cce418b41311937ee160fbf130df3ca6abe6
Sebastian Pech <sebastian.pech@me.com>
2018-09-01 11:30:36 +0200

    Change behavior of @post
    @post now enforces it's function on the return argument if no specific
    variable names where added

--- src/PrePostCall.jl
+++ src/PrePostCall.jl
@@ -4,10 +4,17 @@ import MacroTools: splitdef,combinedef,postwalk
 
 export @pre,@post
 
-function get_args_for_call(x::Array{Any,1})
+function get_qargs_for_call(x::Array{Any,1})
     args = map(x) do a
         isa(a,Expr) && return Expr(:quote,:($(a.args[1])))
         return Expr(:quote,:($a))
+    end
+end
+
+function get_args_for_call(x::Array{Any,1})
+    args = map(x) do a
+        isa(a,Expr) && return (a.args[1])
+        return a
     end
 end
 
@@ -15,12 +22,12 @@ macro pre(cfun)
     def = splitdef(cfun)
     return esc(quote
         $cfun
-           macro $(def[:name])(fun)
+        macro $(def[:name])(fun)
            if fun.head == :macrocall
                fun = macroexpand(@__MODULE__,fun)
            end
            _def = PrePostCall.splitdef(fun)
-           insert!(_def[:body].args,1,Expr(:call,$(def[:name]),$(PrePostCall.get_args_for_call(def[:args])...)))
+           insert!(_def[:body].args,1,Expr(:call,$(def[:name]),$(PrePostCall.get_qargs_for_call(def[:args])...)))
            esc(PrePostCall.combinedef(_def))
         end
         macro $(def[:name])(arg,fun)
@@ -36,32 +43,44 @@ end
 
 macro post(cfun)
     def = splitdef(cfun)
+    retExpr = length(def[:args])==1 ? :(Expr(:call,$(def[:name]),:($ret))) : :(Expr(:call,$(def[:name]),Expr(:...,:($ret))))
     return esc(quote
         $cfun
         macro $(def[:name])(fun)
             if fun.head == :macrocall
                 fun = macroexpand(@__MODULE__,fun)
             end
-            _def = PrePostCall.splitdef(fun)
-            _def[:body] = PrePostCall.postwalk(_def[:body]) do l
-                if isa(l,Expr) && l.head == :return
-                    return Expr(:block,Expr(:call,$(def[:name]),$(PrePostCall.get_args_for_call(def[:args])...)),l)
-                end
-                return l
-            end
+             _def = PrePostCall.splitdef(fun)
+             _fun = copy(_def)
+             ret = gensym("ret")    
+               name = gensym(_def[:name])
+               _fun[:name] = name
+             _def[:body] = Expr(:block,
+                              PrePostCall.combinedef(_fun),
+                              Expr(Symbol("="),:($ret),Expr(:call,:($name),:($(PrePostCall.get_args_for_call(_def[:args])...)))),
+                                $retExpr,
+                                Expr(:return,:($ret))                    
+                              )
             esc(PrePostCall.combinedef(_def))
         end
-        macro $(def[:name])(arg,fun)
+               macro $(def[:name])(arg,fun)
+            arg = isa(arg,Symbol) ? arg : Expr(:...,arg)
             if fun.head == :macrocall
                 fun = macroexpand(@__MODULE__,fun)
             end
-            _def = PrePostCall.splitdef(fun)
+               _def = PrePostCall.splitdef(fun)
+               found = 0
             _def[:body] = PrePostCall.postwalk(_def[:body]) do l
                 if isa(l,Expr) && l.head == :return
-                    return Expr(:block,Expr(:call,$(def[:name]),Expr(:...,:($arg))),l)
+                    found += 1
+                    return Expr(:block,Expr(:call,$(def[:name]),:($arg)),l)
                 end
                 return l
             end
+             # No return statement was found add the call after the last expression
+             if found == 0
+               push!(_def[:body].args,Expr(:block,Expr(:call,$(def[:name]),:($arg)),:nothing))
+              end
             esc(PrePostCall.combinedef(_def))
         end
     end)
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,7 @@ struct NSmallerException <: Exception end
 struct NLargerException <: Exception end
 struct NZeroException <: Exception end
 struct BeforeException <: Exception end
+struct FixLengthException <: Exception end
 
 @pre function positive(x::Int)
     x<0 && throw(NNegativeException())
@@ -21,6 +22,14 @@ end
 
 @post function smaller(x,y)
     x>y && throw(NLargerException())
+end
+
+@post function fixLength(v::Vector{Float64})
+    length(v)>3 && throw(FixLengthException())
+end
+
+@post function multret(x,y)
+    x>y && throw(NSmallerException())
 end
 
 module Level1
@@ -85,12 +94,11 @@ end
 
     @testset "Post-Single" begin
         @not0 function foo(x::Int,y::T) where T<:Number
-            res = x^y
-            return res
+            x^y
         end
         @not0 a function foo2(x::Int,y::T) where T<:Number
             a = x^y
-            return a
+            return 2*a
         end
         @not0 a function foo3(x::Int,y::T) where T<:Number
             a = x^y
@@ -99,15 +107,14 @@ end
         end
         @test foo(10,10) == 10^10
         @test_throws NZeroException foo(0,10)
-        @test foo2(10,10) == 10^10
+        @test foo2(10,10) == 2*10^10
         @test_throws NZeroException foo2(0,10)
         @test_throws BeforeException foo3(0,10)
     end
 
     @testset "Post-Two" begin
-        @smaller function foo(x::Int,a::T) where T<:Number
-            y = x^a
-            return y
+        @smaller x,a function foo(x::Int,a::T) where T<:Number
+            return x^a
         end
         @smaller res,con function foo2(x::Int,y::T) where T<:Number
             con = 100
@@ -119,21 +126,38 @@ end
         @test foo2(5,2) == 5^2
         @test_throws NLargerException foo2(10,10)
     end
-
     @testset "Pre-Post" begin
-        @not0 a @larger y,x function foo(x::Int,y::T) where T<:Number
-            a = x^y
-            return a
+        @not0 @larger y,x function foo(x::Int,y::T) where T<:Number
+            return x^y
         end
         @test foo(2,3) == 2^3
         @test_throws NSmallerException foo(2,1) 
         @test_throws NZeroException foo(0,2) 
     end
-
-
     @testset "Module" begin
         @test_throws Main.Level1.InModException Level1.foo(1)
         @test_throws Main.Level1.InModException Level1.bar(1)
         Main.Level1.baz(1)
     end
+    @testset "No return" begin
+        v = [1.0,2.0,3.0]
+        @fixLength v function foo!(v)
+            push!(v,1.0)
+        end
+        @fixLength v function baz!(v)
+            v[1] = 2.0
+        end
+        @test baz!(v) == nothing
+        @test_throws FixLengthException foo!(v)
+    end
+    
+    @testset "Multiple Return" begin
+        @multret function foo(x,y)
+            return x,y
+        end
+        @test_throws NSmallerException foo(2,1)
+        @test foo(1,2) == (1,2)
+    end
+    
 end
+
